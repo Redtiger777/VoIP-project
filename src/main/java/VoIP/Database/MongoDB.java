7112162e1824f4352b20abe3e44ec37b7e5cc976
Shaun Oosthuizen <shaun.oosthuizen.wp@gmail.com>
2020-12-03 13:56:55 +0200

    Feat: Added support for username querying

--- src/main/java/VoIP/Database/MongoDB.java
+++ src/main/java/VoIP/Database/MongoDB.java
@@ -1,5 +1,6 @@
 package VoIP.Database;
 
+// MongoDB imports
 import com.mongodb.client.*;
 import org.bson.Document;
 import java.util.ArrayList;
@@ -9,15 +10,41 @@ import static com.mongodb.client.model.Projections.*;
 import static com.mongodb.client.model.Sorts.descending;
 import java.util.logging.Logger;
 import java.util.logging.Level;
+// PrintOut class import
+import VoIP.Misc.PrintOut;
 
 public class MongoDB {
-    public static void main(String[] args) {
+    // NOTE: Never Commit with a visible uri!!!!
+        private static final String CONNECT_URI =
+    "mongodb+srv://admin:0kAUprVNKpKQCiuq@voip-cluster.y7f8o.mongodb.net/DB";
 
+    public static void main(String[] args) {
         Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
         mongoLogger.setLevel(Level.SEVERE);
 
-        String connectionString = "";
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+        try (MongoClient mongoClient = MongoClients.create(CONNECT_URI)) {
+            PrintOut.printInfo("Database connection success!");
+        } catch (Exception e){
+            PrintOut.printError("Database connection failed!");
+        }
+    }
+
+    public static boolean checkLoginCred(String givenUser, String givenPass) {
+        boolean correctPass = false;
+        boolean correctUser = false;
+        Document queryUser = getUser(givenUser);
+        if (queryUser != null) {
+            if (queryUser.get("username").equals(givenUser)) {
+                correctUser = true;
+            }
+        }
+        return correctUser;
+    }
+    private static Document getUser(String username) {
+        Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+        mongoLogger.setLevel(Level.SEVERE);
+
+        try (MongoClient mongoClient = MongoClients.create(CONNECT_URI)) {
             // Make connection to the databse
             MongoDatabase testDB = mongoClient.getDatabase("Test_DB");
 
@@ -25,10 +52,10 @@ public class MongoDB {
             MongoCollection<Document> myCollection = testDB.getCollection("Test_Collection");
 
             // Save the required query in a "Document" object
-            Document user = myCollection.find(new Document("username", "Willem")).first();
-
-            // Print the user
-            System.out.println("Queried user: " + user.toJson());
+            Document user = myCollection.find(new Document("username", username)).first();
+            return user;
+        } catch (Exception e) {
+            return null;
         }
     }
 }
